1a954502f5a0254807846d38e237cfcf24923093
liuwllm <liu.wllm@gmail.com>
2023-01-01 23:01:57 -0500

    Add functions to extract kanji from string

--- pdf.py
+++ pdf.py
@@ -2,8 +2,11 @@ import tika
 tika.initVM()
 from tika import parser
 
+from kanji import *
+
 parsed = parser.from_file('test.pdf')
 
 parsed['content'] = parsed['content'].strip().replace('\n', '')
 
-print(parsed)
+print(findKanji(parsed['content']))
+
