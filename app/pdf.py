e4d8ffd37809213c1639ba7ecd7538aa117e2b55
liuwllm <liu.wllm@gmail.com>
2023-01-02 19:46:46 -0500

    Add missing files

---
+++ app/pdf.py
@@ -0,0 +1,11 @@
+import tika
+tika.initVM()
+from tika import parser
+
+from kanji import *
+
+parsed = parser.from_file('./app/test.pdf')
+
+parsed['content'] = parsed['content'].strip().replace('\n', '')
+
+pdfIsolatedKanji = findKanji(parsed['content'])
