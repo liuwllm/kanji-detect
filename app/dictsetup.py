d8e581469cc7e097510f9c6874bf46eb96cebab7
liuwllm <liu.wllm@gmail.com>
2023-01-02 19:30:31 -0500

    Add all misc. files

--- app/dictsetup.py
+++ app/dictsetup.py
@@ -1,13 +1,20 @@
 from pdf import *
 
-frequency = {}
+def convert(isolatedKanji):
+    frequency = {}
 
-for kanji in pdfIsolatedKanji:
-    if kanji in frequency:
-        frequency[kanji] += 1
-    else:
-        frequency[kanji] = 1
+    for kanji in isolatedKanji:
+        if kanji in frequency:
+            frequency[kanji] += 1
+        else:
+            frequency[kanji] = 1
 
-sortedKanji = sorted(frequency.items(), key = lambda x : x[1], reverse = True)
-convertedSortedKanji = dict(sortedKanji)
-print(convertedSortedKanji)
+    sortedKanji = sorted(frequency.items(), key = lambda x : x[1], reverse = True)
+    convertedSortedKanji = dict(sortedKanji)
+    return convertedSortedKanji
+
+'''
+with open('./app/kanji.txt', 'w') as text:
+    for kanji in convertedSortedKanji:
+        text.write(kanji+'\n')
+'''
